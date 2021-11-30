7afd50dc5180cdb412deedfd8b05db3340588b9e
Puker Who <nawwesele@gmail.com>
2021-11-30 13:52:26 +0200

    edit

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
   <!-- Treba links -->
   <div class="container mx-auto px-2 lg:px-5  mb-5">
     <h3 class="text-2xl text-gray-700 font-bold mb-2"><?php _e('Популярные сайты', 'tarakan'); ?></h3>
-    <div class="treba-links flex items-center bg-white border border-gray-300 py-2 px-4">
+    <div class="treba-links flex flex-wrap items-center bg-white border border-gray-300 py-2 px-4">
       <?php do_shortcode('[render-treba-links]'); ?>
       <?php echo do_shortcode('[render-treba-top-links]'); ?>  
     </div>
