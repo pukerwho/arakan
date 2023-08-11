8577755937da27ec86b8d9f38ac0feab6c469d6d
Puker Who <nawwesele@gmail.com>
2023-08-12 00:26:10 +0300

    Edit

--- page-add.php
+++ page-add.php
@@ -67,10 +67,10 @@ get_header();
 <?php 
   
     echo "test";
-    $chatID = "@tarakanadd";
+    $chatID = "tarakanadd";
     $apiToken = carbon_get_theme_option("crb_telegram_api");
     $data = [
-      'chat_id' => $chatID, 
+      'chat_id' => "@tarakanadd", 
       'text' => "Hi!",
     ];
     $response = file_get_contents("https://api.telegram.org/bot".$apiToken."/sendMessage?" . http_build_query($data) );    
