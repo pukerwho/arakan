93b2dbb8ac57c05a164c5030beb29a861ac6ede7
Puker Who <nawwesele@gmail.com>
2023-11-21 11:07:07 +0200

    edit

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package G-Info
- */
-
-get_header();
-?>
+<?php get_header(); ?>
 
 	<main id="primary">
 		<div class="welcome min-h-screen bg-gradient-to-r from-indigo-600 to-indigo-400 pt-20 lg:pt-52 pb-36 lg:pb-0">
